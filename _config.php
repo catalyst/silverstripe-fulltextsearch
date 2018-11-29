52f8ca8a8a820e6f1c6697ef74f827992874eca6
Elliot Sawyer <elliotsawyer@catalyst.net.nz>
2018-11-29 16:46:17 +1300

    MINOR: Solr5  with Silverstripe 3.7 support

--- _config.php
+++ _config.php
@@ -1,5 +1,5 @@
 <?php
-
+if (!class_exists('SS_Object')) class_alias('Object', 'SS_Object');
 global $databaseConfig;
 if (isset($databaseConfig['type'])) SearchUpdater::bind_manipulation_capture();
 
